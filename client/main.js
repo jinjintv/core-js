a242cc8cbd65d32bd0961ef82646c1c4ddfb5268
jinjintv <theffffff@naver.com>
2024-07-03 11:03:10 +0900

    dd

--- client/main.js
+++ client/main.js
@@ -1,24 +1,45 @@
-
-
-class MyElement extends HTMLElement{
+class Button extends HTMLElement {
 
     constructor(){
-        super();
+      super();
+  
+      this.button = document.querySelector('button');
+  
     }
-
+  
     connectedCallback(){
-        console.log('탄생함');
+      this._render();
     }
-
+  
     disconnectedCallback(){
-        console.log('죽음');
+  
     }
-}
-
-customElements.define('c-element',MyElement);
-
-const elem = document.createElement('c-element');
-
-const app = document.getElementById('app');
-
-app.appendChild(elem)
+  
+    static get observedAttributes(){
+      return ['id'];
+    }
+  
+    attributeChangedCallback(name,oldValue,newValue){
+      if(oldValue !== newValue){
+        this._render()
+      }
+    }
+  
+    _render(){
+      this.button.textContent = this.id;
+    }
+  
+  }
+  
+  
+  customElements.define('c-button',Button);
+  
+  
+  
+  const c = document.querySelector('c-button');
+  
+  let count = 0;
+  
+  c.addEventListener('click',()=>{
+    c.setAttribute('id',++count)
+  })
