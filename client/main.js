174ab33905852c10e722fbad11f6f89dff404b40
jinjintv <theffffff@naver.com>
2024-07-03 11:38:52 +0900

    1

--- client/main.js
+++ client/main.js
@@ -1,45 +1,15 @@
-class Button extends HTMLElement {
 
-    constructor(){
-      super();
-  
-      this.button = document.querySelector('button');
-  
-    }
-  
-    connectedCallback(){
-      this._render();
-    }
-  
-    disconnectedCallback(){
-  
-    }
-  
-    static get observedAttributes(){
-      return ['id'];
-    }
-  
-    attributeChangedCallback(name,oldValue,newValue){
-      if(oldValue !== newValue){
-        this._render()
-      }
-    }
-  
-    _render(){
-      this.button.textContent = this.id;
-    }
-  
-  }
-  
-  
-  customElements.define('c-button',Button);
-  
-  
-  
-  const c = document.querySelector('c-button');
-  
-  let count = 0;
-  
-  c.addEventListener('click',()=>{
-    c.setAttribute('id',++count)
-  })
+const template = document.createElement('template');
+
+template.innerHTML = `
+<div>bye</div>
+<div>javascript</div>
+`
+
+
+const temp = document.querySelector('#temp');
+const app = document.querySelector('#app');
+
+const clone = template.content.cloneNode(true);
+
+app.appendChild(clone);
